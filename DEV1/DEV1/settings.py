e98ba209e272389b5c35a6fee0bcc4ad06e74413
Luan <2023017361@aluno.restinga.ifrs.edu.br>
2026-05-18 22:25:45 -0300

    18/05

--- DEV1/DEV1/settings.py
+++ DEV1/DEV1/settings.py
@@ -111,6 +111,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = "contas/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
--- DEV1/DEV1/urls.py
+++ DEV1/DEV1/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from DEV1.views.buscar import BuscarView
 from DEV1.views.contato import ContatoView
+from DEV1.views.perfil import PerfilView
 from .views.estaticas import index
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', index, name="index"),
@@ -10,4 +12,7 @@ urlpatterns = [
     path('aula/', include("aula.urls", namespace="aula")),
     path('contato/classe/', ContatoView.as_view(), name="contato_classe"),
     path("busca/classe/", BuscarView.as_view(), name="busca"),
+    path("contas/login/", auth_views.LoginView.as_view(template_name="contas/login.html", authentication_form="" )),
+    path("contas/", include('django.contrib.auth.urls')),
+    path("contas/perfil", PerfilView.as_view(), name="perfil")
 ]
